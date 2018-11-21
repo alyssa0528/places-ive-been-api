5230be86366e1a6d9cfe2789c85909d6f969480c
alyssa0528 <alyssa.kim@gmail.com>
2018-11-21 16:16:06 -0500

    add actions for Attractions Controller

---
+++ app/controllers/api/attractions_controller.rb
@@ -0,0 +1,36 @@
+class Api::AttractionsController < ApplicationController
+
+  def index
+    render json: Attraction.all
+  end
+
+  def show
+    render json: Attraction.find(params[:id])
+  end
+
+  def create
+    attraction = Attraction.new(attraction_params)
+
+    if attraction.save
+      render json: attraction
+    else
+      render json: { message: attraction.errors }, status: 400
+    end
+  end
+
+  def update
+    attraction = Attraction.find(params[:id])
+
+    if attraction.update(attraction_params)
+      render json: attraction
+    else
+      render json: { message: attraction.errors }, status: 400
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :city_id, :year_visited, :img_url, :notes)
+  end
+end
